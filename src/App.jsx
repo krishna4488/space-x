e02772b761cb5338707f29a0fbccaf5236b39870
arjunkrishna4488 <arjunkrishna4488@gmail.com>
2023-09-06 13:29:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi Hello</h1>
+      <input type="password" />
+    </div>
+  );
+}
+
+export default App;
